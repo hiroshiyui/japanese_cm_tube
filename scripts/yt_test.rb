b7d6dda87c7ddfc6df9a1ba845d9e69ca7e5e5b6
Huei-Horng Yo <hiroshi@ghostsinthelab.org>
2018-07-16 20:50:37 +0800

    Export list to *.csv
    Signed-off-by: Huei-Horng Yo <hiroshi@ghostsinthelab.org>

--- scripts/yt_test.rb
+++ scripts/yt_test.rb
@@ -1,10 +1,11 @@
 #!/usr/bin/env ruby
 require "yt"
 require "time"
+require "csv"
 
 raise "Missing YT_API_KEY environment variable!" if ENV["YT_API_KEY"].nil?
 
-def list_japanese_cm_videos(period = today)
+def get_japanese_cm_videos(period = today)
   videos = Yt::Collections::Videos.new
   videos.where(
     q: "CM 動画",
@@ -13,8 +14,18 @@ def list_japanese_cm_videos(period = today)
     order: "date",
     publishedAfter: period[:from],
     publishedBefore: period[:to],
-  ).each do |v|
-    puts "#{v.title} - https://www.youtube.com/watch?v=#{v.id}"
+  )
+end
+
+def save_japanese_cm_videos(period = today)
+  videos = get_japanese_cm_videos(period)
+
+  CSV.open("#{period[:from]}_#{period[:to]}.csv", "wb", :force_quotes => true) do |csv|
+    csv << ["title", "url"]
+    videos.each do |v|
+      csv << [v.title, video_url(v.id)]
+      puts "#{v.title} - #{video_url(v.id)}"
+    end
   end
 end
 
@@ -32,4 +43,8 @@ def this_month
   {:from => from, :to => now}
 end
 
-list_japanese_cm_videos(this_month)
+def video_url(id)
+  "https://www.youtube.com/watch?v=#{id}"
+end
+
+save_japanese_cm_videos
